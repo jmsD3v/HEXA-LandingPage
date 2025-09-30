edea2e62f643dc6ea36e2e6723b16bddfd65a706
Juan Manuel Silva <juanmanuelsilva06@gmail.com>
2025-09-30 10:17:46 -0300

    correccion en el logo del hero

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -26,18 +26,13 @@ export default function hero() {
         </div>
         <div className='flex justify-center items-center max-md:h-auto py-8 md:py-0 md:h-full relative z-10'>
           {' '}
-          <picture
-            className='w-40 sm:w-48 md:w-56 lg:w-60 mx-auto md:mr-10 lg:mr-10 aspect-square'
+          <img
+            src='/logo-HEXA-hero.webp'
+            alt='Logo de HEXA Servicios Integrales'
+            className='invert w-40 sm:w-48 md:w-56 lg:w-60 mx-auto md:mr-10 lg:mr-10 aspect-square object-contain'
             data-aos='fade-up'
             data-aos-delay='200'
-          >
-            <source srcSet='/logo-HEXA-white.webp' media='(prefers-color-scheme: dark)' />
-            <img
-              src='/logo-HEXA-hero.webp'
-              alt='Logo de HEXA Servicios Integrales'
-              className='w-full h-full object-contain'
-            />
-          </picture>
+          />
         </div>
         {/* <div className='flex flex-col justify-center items-center z-10 max-md:mt-10'>
           <div
